50be82d51d719b94fa58490431a50054f31b8762
Dario Teixeira <dte@issuu.com>
2018-08-16 14:36:53 +0200

    Drop no longer needed functions from Ppx_mysql_runtime

--- lib/ppx_mysql_runtime.ml
+++ lib/ppx_mysql_runtime.ml
@@ -17,19 +17,3 @@ let get_option = function
     | Some x -> x
     | None   -> assert false (* FIXME *)
 
-let select_one = function
-    | [x] -> Ok x
-    | []  -> Error `Found_none_expected_one
-    | _   -> Error `Found_many_expected_one
-
-let select_opt = function
-    | [x] -> Ok (Some x)
-    | []  -> Ok None
-    | _   -> Error `Found_many_expected_maybe_one
-
-let select_all xs =
-    Ok xs
-
-let execute = function
-    | [] -> Ok ()
-    | _  -> Error `Found_nonzero_expected_zero
--- lib/ppx_mysql_runtime.mli
+++ lib/ppx_mysql_runtime.mli
@@ -13,10 +13,3 @@ val map_option: ('a -> 'b) -> 'a option -> 'b option
 
 val get_option: 'a option -> 'a
 
-val select_one: 'a list -> ('a, [> `Found_none_expected_one | `Found_many_expected_one ]) result
-
-val select_opt: 'a list -> ('a option, [> `Found_many_expected_maybe_one ]) result
-
-val select_all: 'a list -> ('a list, unit) result
-
-val execute: 'a list -> (unit, [> `Found_nonzero_expected_zero ]) result
