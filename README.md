ddb2dfe8ff17cead738f992d81f3fc85fde27b58
Roddy <github@roddymacsween.co.uk>
2019-12-17 20:34:05 +0000

    Updated README

--- README.md
+++ README.md
@@ -1,4 +1,42 @@
-ppx_rapper
-=========
+# ppx_rapper
 
-Like ppx\_mysql (uses the same query syntax) but with Caqti/PostgreSQL/LWT as the backend and a few more features.
+Like ppx\_mysql (uses the same query syntax) but with Caqti/PostgreSQL/LWT as the backend.
+
+## Use
+```ocaml
+let my_query =
+  [%rapper
+    get_opt
+      {sql|
+      SELECT @int{id}, @string{username}, @bool{following}, @string?{bio}
+      FROM users
+      WHERE username <> %string{wrong_user} AND id > %int{min_id}
+      |sql}]
+```
+turns into
+
+```ocaml
+let my_query =
+  let request =
+    Caqti_request.find_opt
+      Caqti_type.(tup2 string int)
+      Caqti_type.(tup2 int (tup2 string (tup2 bool (option string))))
+      {sql|
+      SELECT id, username, following, bio
+      FROM users
+      WHERE username <> ? AND id > ?
+      |sql}
+  in
+  let query (module Db : Caqti_lwt.CONNECTION) ~wrong_user ~min_id =
+    Db.find_opt query (wrong_user, min_id)
+  in
+  query
+```
+
+Query functions are
+- `execute` for queries that return 0 rows, represented as `()`
+- `get_one` for queries that return 1 rows, represented as a tuple
+- `get_opt` for queries that return 0 or 1 rows, represented as a tuple option
+- `get_many` for queries that many return any number of rows, represented as a list of tuples
+
+These correspond to `exec`, `find`, `find_opt` and `collect` in `Caqti_request`.
--- test/rewriter/test.expected.ml
+++ test/rewriter/test.expected.ml
@@ -2,210 +2,105 @@ let many_arg_execute =
   let query =
     Caqti_request.exec
       (let open Caqti_type in
-      tup2 string (tup2 string (tup2 (option string) int)))
-      "\n\
-      \      UPDATE users\n\
-      \      SET (username, email, bio) = (?, ?, ?)\n\
-      \      WHERE id = ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username ~email ~bio ~id =
-    Db.exec query (username, (email, (bio, id)))
-  in
+         tup2 string (tup2 string (tup2 (option string) int)))
+      "\n      UPDATE users\n      SET (username, email, bio) = (?, ?, ?)\n      WHERE id = ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username 
+    ~email  ~bio  ~id  = Db.exec query (username, (email, (bio, id))) in
   wrapped
-
 let single_arg_execute =
   let query =
-    Caqti_request.exec
-      (let open Caqti_type in
-      string)
-      "\n      UPDATE users\n      SET username = ?\n      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username =
-    Db.exec query username
-  in
+    Caqti_request.exec (let open Caqti_type in string)
+      "\n      UPDATE users\n      SET username = ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username  =
+    Db.exec query username in
   wrapped
-
 let no_arg_execute =
   let query =
-    Caqti_request.exec
-      (let open Caqti_type in
-      unit)
-      "\n      UPDATE users\n      SET username = 'Hello!'\n      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) () = Db.exec query () in
+    Caqti_request.exec (let open Caqti_type in unit)
+      "\n      UPDATE users\n      SET username = 'Hello!'\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) () =
+    Db.exec query () in
   wrapped
-
 let many_arg_get_one =
   let query =
-    (let open Caqti_request in
-    find)
-      (let open Caqti_type in
-      tup2 string int)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE username = ? AND id > ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username ~min_id =
-    Db.find query (username, min_id)
-  in
+    (let open Caqti_request in find) (let open Caqti_type in tup2 string int)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE username = ? AND id > ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username 
+    ~min_id  = Db.find query (username, min_id) in
   wrapped
-
 let single_arg_get_one =
   let query =
-    (let open Caqti_request in
-    find)
-      (let open Caqti_type in
-      string)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE username = ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username =
-    Db.find query username
-  in
+    (let open Caqti_request in find) (let open Caqti_type in string)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE username = ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username  =
+    Db.find query username in
   wrapped
-
 let no_arg_get_one =
   let query =
-    (let open Caqti_request in
-    find)
-      (let open Caqti_type in
-      unit)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n      SELECT id, username\n      FROM users\n      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) () = Db.find query () in
+    (let open Caqti_request in find) (let open Caqti_type in unit)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) () =
+    Db.find query () in
   wrapped
-
 let many_arg_get_one_repeated_arg =
   let query =
-    (let open Caqti_request in
-    find)
-      (let open Caqti_type in
-      tup2 int (tup2 string int))
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE id = ? OR username = ? OR id <> ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~id ~username =
-    Db.find query (id, (username, id))
-  in
+    (let open Caqti_request in find)
+      (let open Caqti_type in tup2 int (tup2 string int))
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE id = ? OR username = ? OR id <> ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~id  ~username 
+    = Db.find query (id, (username, id)) in
   wrapped
-
 let many_arg_get_opt =
   let query =
-    (let open Caqti_request in
-    find_opt)
-      (let open Caqti_type in
-      tup2 string int)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE username = ? AND id > ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username ~min_id =
-    Db.find_opt query (username, min_id)
-  in
+    (let open Caqti_request in find_opt)
+      (let open Caqti_type in tup2 string int)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE username = ? AND id > ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username 
+    ~min_id  = Db.find_opt query (username, min_id) in
   wrapped
-
 let single_arg_get_opt =
   let query =
-    (let open Caqti_request in
-    find_opt)
-      (let open Caqti_type in
-      string)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE username = ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username =
-    Db.find_opt query username
-  in
+    (let open Caqti_request in find_opt) (let open Caqti_type in string)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE username = ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username  =
+    Db.find_opt query username in
   wrapped
-
 let no_arg_get_opt =
   let query =
-    (let open Caqti_request in
-    find_opt)
-      (let open Caqti_type in
-      unit)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n      SELECT id, username\n      FROM users\n      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) () = Db.find_opt query () in
+    (let open Caqti_request in find_opt) (let open Caqti_type in unit)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) () =
+    Db.find_opt query () in
   wrapped
-
 let many_arg_get_many =
   let query =
-    (let open Caqti_request in
-    collect)
-      (let open Caqti_type in
-      tup2 string int)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE username = ? AND id > ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username ~min_id =
-    Db.collect_list query (username, min_id)
-  in
+    (let open Caqti_request in collect)
+      (let open Caqti_type in tup2 string int)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE username = ? AND id > ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username 
+    ~min_id  = Db.collect_list query (username, min_id) in
   wrapped
-
 let single_arg_get_many =
   let query =
-    (let open Caqti_request in
-    collect)
-      (let open Caqti_type in
-      string)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n\
-      \      SELECT id, username\n\
-      \      FROM users\n\
-      \      WHERE username = ?\n\
-      \      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) ~username =
-    Db.collect_list query username
-  in
+    (let open Caqti_request in collect) (let open Caqti_type in string)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      WHERE username = ?\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) ~username  =
+    Db.collect_list query username in
   wrapped
-
 let no_arg_get_many =
   let query =
-    (let open Caqti_request in
-    collect)
-      (let open Caqti_type in
-      unit)
-      (let open Caqti_type in
-      tup2 int string)
-      "\n      SELECT id, username\n      FROM users\n      "
-  in
-  let wrapped (module Db : Caqti_lwt.CONNECTION) () =
-    Db.collect_list query ()
-  in
+    (let open Caqti_request in collect) (let open Caqti_type in unit)
+      (let open Caqti_type in tup2 int string)
+      "\n      SELECT id, username\n      FROM users\n      " in
+  let wrapped ((module Db)  : (module Caqti_lwt.CONNECTION)) () =
+    Db.collect_list query () in
   wrapped
