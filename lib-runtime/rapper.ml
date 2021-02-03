c083ea7e4808c0e029e27ada8b1610a4915d5301
Bikal Lem <gbikal@gmail.com>
2021-02-03 01:24:15 +0000

    Add IO abstractions to lib-runtime

--- lib-runtime/rapper.ml
+++ lib-runtime/rapper.ml
@@ -49,3 +49,48 @@ module Internal = struct
     let add t x (Pack (t', x')) = Pack (Caqti_type.tup2 t' t, (x', x))
   end
 end
+
+module type IO = sig
+  type +'a t
+
+  val return : 'a -> 'a t
+
+  val map : 'a t -> ('a -> 'b) -> 'b t
+
+  module Stream : Caqti_stream.S with type 'a future := 'a t
+end
+
+module type CONTEXT = sig
+  module Rapper_io : sig
+    type +'a future
+
+    val map : 'a future -> ('a -> 'b) -> 'b future
+
+    val fail : 'e -> ('a, 'e) result future
+
+    module Stream : Caqti_stream.S with type 'a future := 'a future
+
+    module type CONNECTION =
+      Caqti_connection_sig.S
+        with type 'a future := 'a future
+         and type ('a, 'err) stream := ('a, 'err) Stream.t
+  end
+end
+
+module Make_context (Io : IO) :
+  CONTEXT
+    with type 'a Rapper_io.future := 'a Io.t
+     and module Rapper_io.Stream = Io.Stream = struct
+  module Rapper_io = struct
+    let map = Io.map
+
+    let fail e = Io.return (Error e)
+
+    module Stream = Io.Stream
+
+    module type CONNECTION =
+      Caqti_connection_sig.S
+        with type 'a future := 'a Io.t
+         and type ('a, 'err) stream := ('a, 'err) Stream.t
+  end
+end
--- lib-runtime/rapper.mli
+++ lib-runtime/rapper.mli
@@ -19,3 +19,35 @@ module Internal : sig
     val add : 'a Caqti_type.t -> 'a -> t -> t
   end
 end
+
+module type IO = sig
+  type +'a t
+
+  val return : 'a -> 'a t
+
+  val map : 'a t -> ('a -> 'b) -> 'b t
+
+  module Stream : Caqti_stream.S with type 'a future := 'a t
+end
+
+module type CONTEXT = sig
+  module Rapper_io : sig
+    type +'a future
+
+    val map : 'a future -> ('a -> 'b) -> 'b future
+
+    val fail : 'e -> ('a, 'e) result future
+
+    module Stream : Caqti_stream.S with type 'a future := 'a future
+
+    module type CONNECTION =
+      Caqti_connection_sig.S
+        with type 'a future := 'a future
+         and type ('a, 'err) stream := ('a, 'err) Stream.t
+  end
+end
+
+module Make_context (Io : IO) :
+  CONTEXT
+    with type 'a Rapper_io.future := 'a Io.t
+     and module Rapper_io.Stream = Io.Stream
